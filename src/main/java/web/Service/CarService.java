f7b5ee5f3a3255cb412f50ad9be128321503e18c
Aleksandr <tarasenko83@bk.ru>
2023-06-11 09:05:13 +0700

    Change CarService.java showCars to showFromOneTillFourCars, delete parameter List<Car> Add method showAllCars

--- src/main/java/web/Service/CarService.java
+++ src/main/java/web/Service/CarService.java
@@ -5,5 +5,6 @@ import web.models.Car;
 import java.util.List;
 
 public interface CarService {
-    public List<Car> showCars(List<Car> list, int count);
+    public List<Car> showAllCars();
+    public List<Car> showFromOneTillFourCars(int count);
 }
--- src/main/java/web/Service/CarServiceImpl.java
+++ src/main/java/web/Service/CarServiceImpl.java
@@ -18,12 +18,13 @@ public class CarServiceImpl implements CarService{
         carsList.add(new Car(++CARS_COUNT, "Mercedes", "600"));
         carsList.add(new Car(++CARS_COUNT, "Opel", "Astra"));
     }
-
+    @Override
+    public List<Car> showAllCars() {
+        return carsList;
+    }
 
     @Override
-    public List<Car> showCars(List<Car> list, int count) {
-//        return list.stream().filter(Car -> Car.getId() <= count).collect(Collectors.toList());
-
-        return count < 5 ?  list.subList(0, count) : list;
+    public List<Car> showFromOneTillFourCars(int count) {
+        return count < 5 ?  showAllCars().subList(0, count) : showAllCars();
     }
 }
