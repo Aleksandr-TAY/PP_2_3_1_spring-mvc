806e24ec0208aa2608c5de5d3a8b69d11688bffa
Aleksandr <tarasenko83@bk.ru>
2023-06-16 19:59:44 +0700

    add new.html, user.html, change users.html, change UsersController(add newUser, create)

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import web.Service.UserService;
+import web.models.User;
 
 @Controller
 public class UsersController {
@@ -23,5 +26,15 @@ public class UsersController {
 	public String showOneUser(@PathVariable("id") int id1, ModelMap model) {
 		model.addAttribute("user", userService.getUser(id1));
 		return "user";
+	}
+
+	@GetMapping("/new")
+	public String newUser(@ModelAttribute("user") User user) {
+		return "new";
+	}
+	@PostMapping()
+	public String create(@ModelAttribute("user") User user) {
+		userService.addUser(user);
+		return "redirect:/";
 	}
 }
